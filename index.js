24e3788bfa589786333b52d229cd778bdba948d4
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-10-31 10:53:27 +0000

    update

--- index.js
+++ index.js
@@ -11,8 +11,13 @@ const friends = [
   { id: 3, name: 'Pere' },
 ]
 
+app.use(express.static(__dirname + '/client/build'));
 app.use(express.json());
 app.use(cors());
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + './client/build/index.html')
+})
 
 app.get('/api/friends', (req, res, next) => {
   res.json(friends);
