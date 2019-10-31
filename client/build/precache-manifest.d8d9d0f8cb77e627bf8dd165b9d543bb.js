7d099a1b06317e92c6072fcb5dd6b3b4c44be2b5
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-10-31 10:50:45 +0000

    send static html

---
+++ client/build/precache-manifest.d8d9d0f8cb77e627bf8dd165b9d543bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc436539c656e8f6de3603ff651e62d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5676f0f42688c9268304",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5e3d748f82a101bb714e",
+    "url": "/static/js/2.29324aab.chunk.js"
+  },
+  {
+    "revision": "5676f0f42688c9268304",
+    "url": "/static/js/main.d4503fc5.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
